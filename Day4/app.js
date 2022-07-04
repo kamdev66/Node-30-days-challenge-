2811c7fd22f759c29ac059a05f8b9d8269358777
Kamdev Kumar <kamdevkumar666@gmail.com>
2022-07-05 00:11:05 +0530

    day4 added

---
+++ Day4/app.js
@@ -0,0 +1,13 @@
+/* const name="kamdev kumar"
+
+module.exports=name;
+
+*/
+
+const obj={
+    fname: 'kamdev',
+    lname:"kumar",
+    age:18
+}
+
+module.exports=obj;
---
+++ Day4/index.js
@@ -0,0 +1,8 @@
+/* 
+const fname=require('./app')
+
+console.log(fname); //kamdev kumar
+*/
+
+const object=require('./app')
+console.log(object);
